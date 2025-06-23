aa02910ef9a90671f75063b457118fd9fcc54df4
Vu Hong Viet <vu.hong.viet@sun-asterisk.com>
2025-06-23 23:07:42 +0700

    feat: add GSAP animations to Hero component for enhanced user experience
    - Implemented initial animations for header, subheading, body, and button elements using GSAP.
    - Added scroll-triggered animations to change background color and animate text elements on scroll.
    - Registered GSAP plugins for use in the Hero component.

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { FC } from "react";
 import type { SliceComponentProps } from "@prismicio/react";
 import type { Content } from "@prismicio/client";
@@ -5,23 +7,89 @@ import type { Content } from "@prismicio/client";
 import { asText } from "@prismicio/client";
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 
-/**
- * Props for `Hero`.
- */
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
-/**
- * Component for "Hero" Slices.
- */
 const Hero: FC<HeroProps> = ({ slice }) => {
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl
+      .set(".hero", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1,
+      })
+      .from(
+        ".hero-subheading",
+        {
+          opacity: 0,
+          y: 30,
+        },
+        "+=0.8",
+      )
+      .from(".hero-body", {
+        opacity: 0,
+        y: 10,
+      })
+      .from(".hero-button", {
+        opacity: 0,
+        y: 10,
+        duration: 0.6,
+      });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTl
+      .fromTo(
+        "body",
+        {
+          backgroundColor: "#FDE407",
+        },
+        {
+          backgroundColor: "#D9F99D",
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 20,
+        opacity: 0,
+      });
+  });
+
   return (
     <Bounded
-      className="hero"
+      className="hero opacity-0"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
